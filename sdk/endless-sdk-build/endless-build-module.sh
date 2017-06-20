3f85e24c1d68297cccdb12a3e609ee35f83be67d
Emmanuele Bassi <ebassi@gnome.org>
2017-06-20 19:36:21 +0100

    sdk: Add build-module option for running under Xvfb
    Some components need Xvfb to run their test suite, so we should add an
    option to set up Xvfb before the build.

--- sdk/endless-sdk-build/endless-build-module.sh
+++ sdk/endless-sdk-build/endless-build-module.sh
@@ -16,11 +16,22 @@ while (($# > 0)); do
   --no-network)
     JHBUILD_ARGS="$JHBUILD_ARGS --no-network"
     ;;
+  --with-xvfb)
+    START_XVFB=1
+    ;;
   *)
     MODULES="$MODULES $1"
     ;;
   esac
   shift
 done
+
+if [ -n "$START_XVFB" ]; then
+        XVFB_WHD=${XVFB_WHD:-1280x720x16}
+        Xvfb :99 -ac -screen 0 $XVFB_WHD -nolisten tcp &
+        xvfb_pid=$!
+
+        export DISPLAY=:99
+fi
 
 jhbuild $JHBUILD_OPTS build $JHBUILD_ARGS $MODULES
